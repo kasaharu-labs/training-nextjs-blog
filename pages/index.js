946839c33d8edacab1334eb464baa95cf6d6b57b
Wataru.Kasahara <kasahara1019@gmail.com>
2020-12-28 13:28:16 +0900

    feat: ファイルシステムからデータを読み込んで表示

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
@@ -12,6 +23,22 @@ export default function Home() {
         <p>[Your Self Introduction]</p>
         <p>(This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, data, title }) => {
+            return (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {data}
+              </li>
+            )
+          })}
+        </ul>
       </section>
     </Layout>
   )
